8cf4a74d22d96b019aa936f7b5c227a9b7b786d7
Kary1661 <posposkarina@gmail.com>
2023-11-04 15:33:50 -0300

    last

--- game.js
+++ game.js
@@ -1,71 +1,77 @@
-let currentMole;
-let currentPlant;
+let currMoleTile;
+let currPlantTile;
 let score = 0;
 let gameOver = false;
 
-window.onload = function(){
-    setGame()
+window.onload = function() {
+    setGame();
 }
-function setGame(){
-    for(let i=0; i>9; i++){
-        let tile = document.createElement('div')
+
+function setGame() {
+    //set up the grid in html
+    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
+        //<div id="0-8"></div>
+        let tile = document.createElement("div");
         tile.id = i.toString();
-        tile.addEventListener('click', selectTile);
-        document.getElementById("board").appendChild(tile)
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000);
-    setInterval(setPlant, 2000);
+    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
+    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
 }
 
-function getRandomTile(){
-    let num = Math.floor(Math.random()*9);
-    return num.toString;
+function getRandomTile() {
+    //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
 }
 
-function setMole(){
-    if(gameOver){
+function setMole() {
+    if (gameOver) {
         return;
     }
-
-    if(currentMole){
-        currentMole.innerHTML = "";
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
     }
-   let mole = document.createElement("img");
-   mole.src = "./monty-mole.png";
-
-   let num = getRandomTile();
-   if(currentMole && currentMole.id == num){
-    return
-   }
-   currentMole = document.getElementById(num);
-   currentMole.appendChild(mole);
-}
-
-function setPlant(){
-    if(gameOver){
-        return;
-    }
-    if(currentPlant) {
-        currentPlant.innerHTML = "";
-    }
-
-    let plant = document.createElement("img");
-    plant.src = "./piranha-plant.png"
+    let mole = document.createElement("img");
+    mole.src = "./monty-mole.png";
 
     let num = getRandomTile();
-    currentPlant = document.getElementById(num);
-    currentPlant.appendChild(plant);
-}
-
-function selectTile(){
-    if(gameOver){
+    if (currPlantTile && currPlantTile.id == num) {
         return;
     }
-    if(this == currentMole){
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "./piranha-plant.png";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    if (this == currMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString();
-    } else if(this == currentPlant){
-        document.getElementById("score").innerText = "GAME OVER:" + score.toString();
+        document.getElementById("score").innerText = score.toString(); //update score html
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
         gameOver = true;
     }
 }
